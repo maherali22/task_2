69c0354de7ad0e3b25950f821ea9667021095da0
Maher <mahiralimattummal@gmail.com>
2025-01-16 23:41:44 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Example from "./example";
 import Content from "./frontend";
 
 function App() {
   return (
     <div>
-      <Content />
+      {/* <Content /> */}
+      <Example />
     </div>
   );
 }
---
+++ src/example.jsx
@@ -0,0 +1,10 @@
+const Example = () => {
+  return (
+    <div className="m-4 grid grid-cols-2 sm:grid-cols-2 gap-4 ">
+      <div className="min-h-[100px] rounded-lg shadow bg-orange-500"></div>
+      <div className="min-h-[200px] rounded-lg shadow bg-green-500"></div>
+    </div>
+  );
+};
+
+export default Example;
