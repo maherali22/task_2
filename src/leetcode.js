ed15102978f42e0f9c7a05ee5f48c49377e20a79
Maher <mahiralimattummal@gmail.com>
2025-01-19 23:52:50 +0530

    letcode

--- src/leetcode.js
+++ src/leetcode.js
@@ -55,4 +55,17 @@ var singleNumber = function (nums) {
 };
 console.log(singleNumber([4, 1, 2, 1, 2]));
 
+//question: 217. Contains Duplicate
+
+var containsDuplicate = function (nums) {
+  let set = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) return true;
+    set.add(nums[i]);
+  }
+  return false;
+};
+console.log(containsDuplicate([1, 2, 3, 1]));
+
+
 
